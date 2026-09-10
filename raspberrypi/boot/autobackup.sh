4eeb7a756e6f2039067f9de4ba5f3ca20bf48bd4
Gabriel <gabriel.brown@yale.edu>
2026-09-10 16:54:55 -0400

    lowercase c to capital C

--- raspberrypi/boot/autobackup.sh
+++ raspberrypi/boot/autobackup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp //boot/firmware/config.txt //home/stu3340/CPSC3340/raspberrypi/boot/config.txt
 hostname -I > //home/stu3340/CPSC3340/raspberrypi/.config/ip.md
-git -c //home/stu3340/CPSC3340 commit -am "auto updated files and IP address"
-git -c //home/stu3340/CPSC3340 push origin main
+git -C //home/stu3340/CPSC3340 commit -am "auto updated files and IP address"
+git -C //home/stu3340/CPSC3340 push origin main
