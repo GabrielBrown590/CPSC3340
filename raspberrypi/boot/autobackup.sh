cb5c94ba262d4192279371f098d48f0d1058dc66
Pi auto update <gabriel.brown@yale.edu>
2026-09-14 16:45:29 -0400

    auto updated files and IP address

--- raspberrypi/boot/autobackup.sh
+++ raspberrypi/boot/autobackup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cp //boot/firmware/config.txt //home/stu3340/CPSC3340/raspberrypi/boot/config.txt
+cp //boot/autobackup.sh //home/stu3340/CPSC3340/raspberrypi/boot/autobackup.sh
+cp //etc/systemd/system/autobackup.service //home/stu3340/CPSC3340/raspberrypi/etc/systemd/system/autobackup.service
 hostname -I > //home/stu3340/CPSC3340/raspberrypi/.config/ip.md
+
 git -C //home/stu3340/CPSC3340 commit -am "auto updated files and IP address"
 git -C //home/stu3340/CPSC3340 push origin main
