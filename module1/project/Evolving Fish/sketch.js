314e5acaa2aec53ffd501cb69c418e204e4b5fa5
Gabriel <gabriel.brown@yale.edu>
2026-09-16 17:07:36 -0400

    fish now have tails

--- module1/project/Evolving Fish/sketch.js
+++ module1/project/Evolving Fish/sketch.js
@@ -1,49 +1,49 @@
     class Fish
     {
-        constructor(type,
-            x, y, //x and y coodinates of fish
-            w, h, //width and height of fish
-            c1RadiusFacter, c2RadiusFacter, c3RadiusFacter, c4RadiusFacter, //determines roundness of each corner as a percentage of the fish size. Only matters for rectangular fish
-            eyesizeFactor, //size of eye of fish as fraction of whichever is smaller, height or width
-            eyeOffsetx, eyeOffsety, //amount interpreted from start to end of fish for eye position. Ex. 0.5, 0.5 is middle of fish. 0.1, 0.1 is nearer the top left
-            color)
+        constructor(options)
         {
             //type. Either e for elliptical or r for rectangualar
-            this.type = type
+            this.type = options.type
 
-            //Fish location and dimensions.
-            this.x = x;
-            this.y = y;
-            this.width = w
-            this.height = h
+            //Fish location and dimensions in pixels.
+            this.x = options.x;
+            this.y = options.y;
+            this.width = options.w
+            this.height = options.h
 
-            //caculate corner radii
-            this.c1Radius=c1RadiusFacter * min(this.height, this.width);
-            this.c2Radius=c2RadiusFacter * min(this.height, this.width);
-            this.c3Radius=c3RadiusFacter * min(this.height, this.width);
-            this.c4Radius=c4RadiusFacter * min(this.height, this.width);
+            //caculate corner radii. CRadiusFactor is a value between 0 and 1 representing radius size relative to size of fish
+            this.c1Radius=options.c1RadiusFactor * min(this.height, this.width);
+            this.c2Radius=options.c2RadiusFactor * min(this.height, this.width);
+            this.c3Radius=options.c3RadiusFactor * min(this.height, this.width);
+            this.c4Radius=options.c4RadiusFactor * min(this.height, this.width);
 
-            //calculate the center coodinates
-            if(this.type == "e")
-                {
-                    this.centerx = x;
-                    this.centery = y;
-                }
-            else if (this.type =="r")
-                {
-                    this.centerx = x + 0.5*this.width;
-                    this.centery = y + 0.5*this.height;
-                }
+            //set the center coodinates
+            this.centerx = options.x;
+            this.centery = options.y;
+                
             
             //calculate eye location
-            this.eyex = lerp(this.centerx-0.5*this.width, this.centerx+0.5*this.width, eyeOffsetx);
-            this.eyey = lerp(this.centery-0.5*this.height, this.centery+0.5*this.height, eyeOffsety);
+            this.eyex = lerp(this.centerx-0.5*this.width, this.centerx+0.5*this.width, options.eyeOffsetx);
+            this.eyey = lerp(this.centery-0.5*this.height, this.centery+0.5*this.height, options.eyeOffsety);
         
             //calculate eye size in pixels
-            this.eyeSize = eyesizeFactor * min(this.height, this.width);
+            this.eyeSize = options.eyeSizeFactor * min(this.height, this.width);
+            
+
+
+            //set all tail factors
+
+            //sets tail type, either wide where the 2 points of triangle are outward or narrow where the 2 points are inwards
+            this.tailType = options.tail.type;
+
+            //calculate the tail points. innerxFactor is how far into the fish the tail starts. outerxFactor is how far out the tail goes. spreadfactor is how far spread the points of the fish tail are.
+            this.tailInnerx = this.x - options.tail.innerxFactor * 0.5  * this.width;
+            this.tailOuterx = this.x- 0.5*this.width - options.tail.outerxFactor * this.width;
+            this.tailTopy = this.y - options.tail.spreadFactor * 0.5 * this.height;
+            this.tailBoty = this.y + options.tail.spreadFactor * 0.5 * this.height;
 
             //color
-            this.color = color;
+            this.color = options.color;
         }
 
         dispay()
@@ -63,42 +63,60 @@
                 {
                     ellipse(this.x, this.y, this.width, this.height);
                 }
+
+
+            //fish tail
+            fill("blue");
+            if (this.tailType == "w")
+                {
+                    //triangle with one point at inner x, and two at outer x. The outer points are at tailTopy and tailBoty
+                    triangle(this.tailInnerx, this.y, this.tailOuterx, this.tailTopy, this.tailOuterx, this.tailBoty)
+                }
+            else if (this.tailType == "n")
+                {
+                    //triangle with one point at outer x and two at inner x. Outer point has y centered and the inner points are at tailTopy and tailBoty
+                    triangle(this.tailInnerx, this.tailTopy, this.tailInnerx, this.tailBoty, this.tailOuterx, this.y)
+                }  
             
             //fish eye
             fill("black")
             circle(this.eyex, this.eyey, this.eyeSize);
 
+            
+
             pop();
         }
     }
+    function generateNormalFish()
+    {
+        
+    }
 
         var testFishR;
-        var testFishE;
+
     function setup() {
         createCanvas(800, 600);
-        testFishR = new Fish(
-            "r",
-            600, 600, //x and y coordinates
-            200, 100, //width and height
-            0.2, 0.2, 0.2, 0.2, //corner radii
-            0.3, //eyesizefactor
-            0.5, 0.5, //eye offset factors
-            "blue", //color
-            );
-            testFishE = new Fish(
-            "e",
-            250, 250, //x and y coordinates
-            200, 100, //width and height
-            0.2, 0.2, 0.2, 0.2, //corner radii
-            0.3, //eyesizefactor
-            0.5, 0.5, //eye offset factors
-            "blue", //color
-            );
-        
+        rectMode(CENTER);
+        testFishR = new Fish
+        ({
+            type: "r",
+            x: 400, y: 400,
+            w: 200, h: 100,
+            c1RadiusFactor: 0.2, c2RadiusFactor: 0.2, c3RadiusFactor: 0.2, c4RadiusFactor: 0.2,
+            eyeSizeFactor: 0.3,
+            eyeOffsetx: 0.5, eyeOffsety: 0.5,
+            tail:{
+                type:"w",
+                innerxFactor:0.5,
+                outerxFactor:0.5,
+                spreadFactor:0.5
+            },
+            color: "blue"
+
+        });        
     }
 
     function draw() {
         background("220");
         testFishR.dispay();
-        testFishE.dispay();
     }
