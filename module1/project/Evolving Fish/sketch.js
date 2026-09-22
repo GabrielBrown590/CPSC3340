16fca67b8c5d4ac90f967da3b63467e90515b111
Gabriel <gabriel.brown@yale.edu>
2026-09-22 19:57:52 -0400

    fine tuning to decrease lag

--- module1/project/Evolving Fish/sketch.js
+++ module1/project/Evolving Fish/sketch.js
@@ -144,10 +144,10 @@
         createCanvas(windowWidth, windowHeight);
 
         pullStrength = map(width, 600, 2000, 0.015, 0.0004, true); //calculate pullstrength based on width of screen
-        numFish = floor(map(width * height, 300000, 3000000, 2, 30, true));//calculate initial num of fish
+        numFish = floor(map(width * height, 300000, 3000000, 2, 20, true));//calculate initial num of fish
         maxFish = numFish * 2;
-        useComplexTailAnimation = (numFish < 25); //if there are too many fish disable complex tail animation to save cpu usage
-        useNeon = (numFish < 30) //if we're bigger than 300,000 pixels disable neon to help pi survive
+        useComplexTailAnimation = (numFish < 20); //if there are too many fish disable complex tail animation to save cpu usage
+        useNeon = (numFish < 20) //if we're bigger than a certain amount, disable neon to help pi survive
 
         rectMode(CENTER);
         colorMode(HSB, 360, 100, 100, 100);
